ea12853352edf9171519328592bf42ba211de927
Todd Churchward <toddchur@amazon.com>
2023-09-02 05:54:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/toddchur-org/s3-bucket/aws"
   version = "2.8.0"
 
-  bucketprefix = "toddchurchward"
+  bucket_prefix = "toddchurchward"
   acl    = "private"
 
   versioning = {
